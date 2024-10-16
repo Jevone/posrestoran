46e5a1d9decbf8bf191d689621c5a0779d0d54fc
Jefri Jaya <jefrijaya08@gmail.com>
2024-10-16 23:02:47 +0700

    set up

---
+++ resources/views/livewire/auth/login.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="card-body">
+        <h3 class="card-title">Selamat Datang</h3>
+        <div class="card-actions">
+            <button class="btn btn-primary">
+                <x-tabler-login class="size-5" />
+                    <span>Login</span>
+            </button>
+        </div>
+    </div>
+</div>
